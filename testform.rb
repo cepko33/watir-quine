4dcae305137b05812f2b226dc28000882c0febdf
Stefan Cepko <cepko33@yahoo.com>
2013-11-23 13:35:24 -0500

    fixed testform I guess

--- testform.rb
+++ testform.rb
@@ -1,5 +1,4 @@
 require "watir-webdriver"
-
 browser = Watir::Browser.new :firefox
 browser.goto "http://lets.qa/test-form/"
 browser.text_field(:name => "vfb-5").set("Stefan")
@@ -8,7 +7,6 @@ browser.text_field(:name => "vfb-8").set("cepko.s@husky.neu.edu")
 browser.text_field(:name => "vfb-7").set("774-922-3901")
 browser.input(:name => "vfb-9[]").click
 browser.input(:name => "vfb-11").click
-
 quine = ["require \"watir-webdriver\"
 browser = Watir::Browser.new :firefox 
 browser.goto \"http://lets.qa/test-form/\" 
@@ -18,8 +16,8 @@ browser.text_field(:name => \"vfb-8\").set(\"cepko.s@husky.neu.edu\")
 browser.text_field(:name => \"vfb-7\").set(\"774-922-3901\")
 browser.input(:name => \"vfb-9[]\").click 
 browser.input(:name => \"vfb-11\").click
-quine = [\"", "browser.text_field(:name => \"vfb-10\").set(quine[0] + quine[0].gsub('\"', '\\\\\\\"') + \"\\\" , \\\"\" + quine[1].gsub('\\\\', '\\\\\\\\\\\\').gsub('\"', '\\\\\\\"') + \"\\\"]\\n\" + quine[1])
+quine = [\"", "
+browser.text_field(:name => \"vfb-10\").set(quine[0] + quine[0].gsub('\"', '\\\\\\\"') + \"\\\" , \\\"\" + quine[1].gsub('\\\\', '\\\\\\\\\\\\').gsub('\"', '\\\\\\\"') + \"\\\"]\\n\" + quine[1])
 browser.text_field(:name => \"vfb-3\").set(\"45\")"]
-
 browser.text_field(:name => "vfb-10").set(quine[0] + quine[0].gsub('"', '\\\"') + "\", \"" + quine[1].gsub('\\', '\\\\\\').gsub('"', '\\\"') + "\"]\n" + quine[1])
 browser.text_field(:name => "vfb-3").set("45")
